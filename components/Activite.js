f441712c3d9168ab7d39c5906ada73c22b015bb0
ousmane <aboousmane@gmail.com>
2021-12-26 18:02:57 +0100

    adding another datetime component to select date on calender

--- components/Activite.js
+++ components/Activite.js
@@ -1,13 +1,28 @@
+import {useState} from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Text} from 'react-native';
+import { Text, View} from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import FontAwesome from 'react-native-vector-icons/FontAwesome5';
-
+import DatePicker from 'react-native-date-picker'
 
 function Sortie(){
+
+  const [date, setDate] = useState(new Date())
+
   return (
-    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Operations de sortie :</Text>
+    <SafeAreaView style={{ flex: 1}}>
+      <View style={{ flex: 1,backgroundColor: 'red' }} ></View>
+      <View style={{ flex: 4, alignItems: 'center' }} >
+
+        <DatePicker 
+          locale='fr'
+          style = {{ height: 120, width : 600, }}
+          androidVariant ="nativeAndroid"
+          mode={"date"}
+          date={date} 
+          onDateChange={setDate} />
+      </View>
+      <View style={{ flex: 6,backgroundColor: '#ccc' }} ></View>
     </SafeAreaView>
   )
 }
